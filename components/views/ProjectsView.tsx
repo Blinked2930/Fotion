d1598ff7d4c6064c5569b7f814716950a85e056e
Emmett Frett <emfrett@gmail.com>
2026-07-29 17:01:47 +0200

    fixes build error

--- components/views/ProjectsView.tsx
+++ components/views/ProjectsView.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useQuery } from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Folder, Inbox, Loader2, GripVertical, GripHorizontal, ArrowRightCircle } from "lucide-react";
 import { getProjectColor } from "./NewTaskForm";
